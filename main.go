8434c9b07cc7f664acafc9133b73959d1ea8d8dc
Vlad Sviatobatko <v.sviatobatko@myfleetsy.com>
2023-02-13 13:09:07 +0200

    added setting server and Shutdown

--- main.go
+++ main.go
@@ -1,26 +1,46 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
+	"github.com/SvyatobatkoVlad/Golang-gRPC/handlers"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request){
-		log.Println("Hello World")
-		d, err := ioutil.ReadAll(r.Body)
-		if err != nil{
-			http.Error(rw, "Ooops", http.StatusBadRequest)
-			return
-		}
-		fmt.Fprintf(rw, "Hello %s", d)
-	})
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
 
-	http.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request){
-		log.Println("Goodbye World")
-	})
+	sm := http.NewServeMux()
+	sm.Handle("/hello", hh)
+	sm.Handle("/goodbye", gh)
 
-	http.ListenAndServe(":9090", nil)
+	s := &http.Server{
+		Addr: ":9090",
+		Handler: sm,
+		IdleTimeout: 120 * time.Second,
+		ReadTimeout: 1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func(){
+	err := s.ListenAndServe()
+	if err != nil{
+		l.Fatal(err)
+	}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <- sigChan
+	l.Println("Recived terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30 * time.Second)
+	s.Shutdown(tc)
 }
